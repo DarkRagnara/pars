7599af14cad742f4c1632c47c0b19636fcbc572c
b.w@gmx.tm <b.w@gmx.tm>
2019-01-01 14:33:53 +0100

    Clarified comment

--- pars.go
+++ pars.go
@@ -14,7 +14,7 @@ import (
 type Parser interface {
 	//Parse is used for the actual parsing. It reads from the reader and returns the result or an error value.
 	//Each parser must remember enough from the call to this method to undo the reading in case of a parsing error that occurs later.
-	//Parse must unread all read bytes in case of an error itself so that another parser could try to parse them.
+	//When Parse returns with an error, Parse must make sure that all read bytes are unread so that another parser could try to parse them.
 	Parse(*reader) (interface{}, error)
 	//Unread puts read bytes back to the reader so that they can be read again by other parsers.
 	Unread(*reader)
