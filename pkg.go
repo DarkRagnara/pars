85af990e7700c5dc868474f9453e0b6578be2ce0
b.w@gmx.tm <b.w@gmx.tm>
2019-01-04 14:06:06 +0100

    golint

--- pkg.go
+++ pkg.go
@@ -1,4 +1,4 @@
-//pars is a simple framework for parser combinators. It is designed to be easy to use, yet powerful enough to allow solving real world problems.
+//Package pars is a simple framework for parser combinators. It is designed to be easy to use, yet powerful enough to allow solving real world problems.
 //Parsers can be arranged into flexible stacks of more elemental parsers. Parser results can be transformed via transformers to easily convert their
 //results into a more fitting format or to implement additional conditions that must be fulfilled for a successful parse.
 //Complex parsers can be debugged by wrapping them with a logger.
