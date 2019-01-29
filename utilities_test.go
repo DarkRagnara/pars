cc8f0b1255adc35ec721ef88b47238883bd1bcb3
b.w@gmx.tm <b.w@gmx.tm>
2019-01-29 13:32:50 +0100

    Added error transformer utility

--- utilities_test.go
+++ utilities_test.go
@@ -52,3 +52,32 @@ func TestSwallowTrailingWhitespace(t *testing.T) {
 	val, err := DiscardLeft(String(" "), DiscardRight(SwallowTrailingWhitespace(Int()), EOF)).Parse(r)
 	assertParse(t, val, err, 123, nil)
 }
+
+func TestErrorTransformer(t *testing.T) {
+	r := stringReader("123")
+	val, err := ErrorTransformer(Int(), func(e error) (interface{}, error) { return 0, nil }).Parse(r)
+	assertParse(t, val, err, 123, nil)
+}
+
+func TestFailingErrorTransformer(t *testing.T) {
+	r := stringReader("a123")
+	expectedErr := fmt.Errorf("Some transformer error")
+	val, err := ErrorTransformer(Int(), func(e error) (interface{}, error) { return nil, expectedErr }).Parse(r)
+	assertParse(t, val, err, nil, expectedErr)
+
+	val, err = ErrorTransformer(Int(), func(e error) (interface{}, error) { return "246", nil }).Parse(r)
+	assertParse(t, val, err, "246", nil)
+
+	val, err = String("a123").Parse(r)
+	assertParse(t, val, err, "a123", nil)
+}
+
+func TestErrorTransformerUnread(t *testing.T) {
+	r := stringReader("123")
+	expectedErr := fmt.Errorf("Forced unread")
+	val, err := DiscardRight(ErrorTransformer(Int(), func(e error) (interface{}, error) { return nil, e }), Error(expectedErr)).Parse(r)
+	assertParse(t, val, err, nil, expectedErr)
+
+	val, err = String("123").Parse(r)
+	assertParse(t, val, err, "123", nil)
+}
