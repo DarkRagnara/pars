f4534576995d6fe76c8dc53b2daaa78b5194b12d
b.w@gmx.tm <b.w@gmx.tm>
2019-02-05 16:59:26 +0100

    Improved calculation example by using Dispatch

--- examples/pars-calc/parser.go
+++ examples/pars-calc/parser.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bitbucket.org/ragnara/pars/v2"
+	"fmt"
 )
 
 //ParseCalculation parses a calculation into an evaler.
@@ -20,27 +21,55 @@ func NewCalculationParser() pars.Parser {
 
 //NewTermParser parses a calculation consisting of added or subtracted calculations of products or a single product.
 func NewTermParser() pars.Parser {
-	return pars.Or(pars.Transformer(pars.Seq(NewProductParser(), NewTermOperatorParser(), pars.Recursive(NewTermParser)), toCalculation), NewProductParser())
-}
-
-//NewTermOperatorParser parses a '+' or '-' sign.
-func NewTermOperatorParser() pars.Parser {
-	return pars.Transformer(pars.SwallowWhitespace(pars.Or(pars.Char('+'), pars.Char('-'))), toOperator)
+	return pars.Dispatch(
+		pars.DescribeClause{calculationClause{pars.Seq(NewProductParser(), NewOperatorParser('+')), pars.Recursive(NewTermParser)}, "addition"},
+		pars.DescribeClause{calculationClause{pars.Seq(NewProductParser(), NewOperatorParser('-')), pars.Recursive(NewTermParser)}, "substraction"},
+		pars.Clause{NewProductParser()})
 }
 
 //NewProductParser parses a calculation consisting of multiplied or divided numbers or a single number.
 func NewProductParser() pars.Parser {
-	return pars.Or(pars.Transformer(pars.Seq(NewNumberParser(), NewPrductOperatorParser(), pars.Recursive(NewProductParser)), toCalculation), NewNumberParser())
+	return pars.Dispatch(
+		pars.DescribeClause{calculationClause{pars.Seq(NewNumberParser(), NewOperatorParser('*')), pars.Recursive(NewProductParser)}, "multiplication"},
+		pars.DescribeClause{calculationClause{pars.Seq(NewNumberParser(), NewOperatorParser('/')), pars.Recursive(NewProductParser)}, "division"},
+		pars.Clause{NewNumberParser()})
+}
+
+type calculationClause []pars.Parser
+
+func (c calculationClause) Parsers() []pars.Parser {
+	return c
+}
+
+func (c calculationClause) TransformResult(atoms []interface{}) interface{} {
+	if len(atoms) != 2 {
+		panic("Exactly two parts expected")
+	}
+
+	subatoms := atoms[0].([]interface{})
+	if len(subatoms) != 2 {
+		panic("Exactly two parts expected")
+	}
+
+	a := subatoms[0].(Evaler)
+	b := atoms[1].(Evaler)
+	op := subatoms[1].(operator)
+
+	return Calculation{a: a, b: b, op: op}
+}
+
+func (c calculationClause) TransformError(err error) error {
+	return err
 }
 
 //NewNumberParser parses a single number.
 func NewNumberParser() pars.Parser {
-	return pars.Transformer(pars.SwallowWhitespace(pars.Float()), toNumber)
+	return pars.Or(pars.Transformer(pars.SwallowWhitespace(pars.Float()), toNumber), pars.Error(fmt.Errorf("number expected")))
 }
 
-//NewPrductOperatorParser parses a '*' or '-' sign.
-func NewPrductOperatorParser() pars.Parser {
-	return pars.Transformer(pars.SwallowWhitespace(pars.Or(pars.Char('*'), pars.Char('/'))), toOperator)
+//NewOperatorParser parses the given rune as an operator.
+func NewOperatorParser(r rune) pars.Parser {
+	return pars.Transformer(pars.SwallowWhitespace(pars.Char(r)), toOperator)
 }
 
 func toOperator(v interface{}) (interface{}, error) {
